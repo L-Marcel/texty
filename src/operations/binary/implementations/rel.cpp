af2088df5998953c251b0a03ffc532dd3202e6f6
Eudes Silva <eudessilvabezerra985@gmail.com>
2026-05-09 18:16:51 -0300

    check-rel

---
+++ src/operations/binary/implementations/rel.cpp
@@ -0,0 +1,57 @@
+#include "rel.hpp"
+
+Type binary_eq_get_type(const Type& left, const Type& right, int line) {
+  if (left == right) {
+    return TypeKind::BOOL;
+  } else {
+    throw error("operação não definida para os respectivos tipos", line);
+  }
+}
+Type binary_not_eq_get_type(const Type& left, const Type& right, int line) {
+  if (left == right) {
+    return TypeKind::BOOL;
+  } else {
+    throw error("operação não definida para os respectivos tipos", line);
+  }
+}
+Type binary_gt_get_type(const Type& left, const Type& right, int line) {
+  if (check_if_is_numeric(left) && check_if_is_numeric(right) &&
+      left == right) {
+    return TypeKind::BOOL;
+  } else {
+    throw error("operação não definida para os respectivos tipos", line);
+  }
+}
+
+Type binary_lt_get_type(const Type& left, const Type& right, int line) {
+  if (check_if_is_numeric(left) && check_if_is_numeric(right) &&
+      left == right) {
+    return TypeKind::BOOL;
+  } else {
+    throw error("operação não definida para os respectivos tipos", line);
+  }
+}
+Type binary_gt_eq_get_type(const Type& left, const Type& right, int line) {
+  if (check_if_is_numeric(left) && check_if_is_numeric(right) &&
+      left == right) {
+    return TypeKind::BOOL;
+  } else {
+    throw error("operação não definida para os respectivos tipos", line);
+  }
+}
+Type binary_lt_eq_get_type(const Type& left, const Type& right, int line) {
+  if (check_if_is_numeric(left) && check_if_is_numeric(right) &&
+      left == right) {
+    return TypeKind::BOOL;
+  } else {
+    throw error("operação não definida para os respectivos tipos", line);
+  }
+}
+Type binary_in_get_type(const Type& left, const Type& right, int line) {
+  Type array_type = Type(TypeKind::ARRAY);
+  if (right == array_type && left == *right.inner_type) {
+    return TypeKind::BOOL;
+  } else {
+    throw error("operação não definida para os respectivos tipos", line);
+  }
+}
