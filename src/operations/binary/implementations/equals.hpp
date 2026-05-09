05f040580ff11544460dbae9d2be06cd4e49e684
Eudes Silva <eudessilvabezerra985@gmail.com>
2026-05-09 18:59:10 -0300

    check-equals-header

---
+++ src/operations/binary/implementations/equals.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "../binary.hpp"
+
+Type binary_eq_get_type(const Type& left, const Type& right, int line);
+Type binary_not_eq_get_type(const Type& left, const Type& right, int line);
