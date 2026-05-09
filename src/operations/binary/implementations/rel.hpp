05542dda9f9298c0f1a79497e58d8a20450a5d43
Eudes Silva <eudessilvabezerra985@gmail.com>
2026-05-09 18:49:13 -0300

    check-rel-header

--- src/operations/binary/implementations/rel.hpp
+++ src/operations/binary/implementations/rel.hpp
@@ -2,8 +2,6 @@
 #include "../../../helpers/numeric.hpp"
 #include "../binary.hpp"
 
-Type binary_eq_get_type(const Type& left, const Type& right, int line);
-Type binary_not_eq_get_type(const Type& left, const Type& right, int line);
 Type binary_gt_get_type(const Type& left, const Type& right, int line);
 Type binary_lt_get_type(const Type& left, const Type& right, int line);
 Type binary_gt_eq_get_type(const Type& left, const Type& right, int line);
