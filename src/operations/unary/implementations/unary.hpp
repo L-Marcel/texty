d0fb92a23c8f0de2b5aa56bd6b422e7a58f67ffc
Tiegow <88515883+Tiegow@users.noreply.github.com>
2026-05-09 21:18:04 -0300

    [feat] check postfix

--- src/operations/unary/implementations/unary.hpp
+++ src/operations/unary/implementations/unary.hpp
@@ -6,3 +6,5 @@ Type unary_minus_get_type(const Type& node, int line);
 Type unary_not_get_type(const Type& node, int line);
 Type unary_rev_get_type(const Type& node, int line);
 Type unary_ref_get_type(const Type& node, int line);
+Type unary_increment_get_type(const Type& node, int line);
+Type unary_decrement_get_type(const Type& node, int line);
