03c014dbda7f992b5508f6dedee7f026c9ee983b
Tiegow <88515883+Tiegow@users.noreply.github.com>
2026-05-11 13:35:38 -0300

    [feat] Add check_bit_or

--- src/operations/binary/binary.cpp
+++ src/operations/binary/binary.cpp
@@ -3,6 +3,7 @@
 #include "implementations/exp.hpp"
 #include "implementations/mult.hpp"
 #include "implementations/or.hpp"
+#include "implementations/bit_or.hpp"
 #include "implementations/rel.hpp"
 #include "implementations/sum.hpp"
 // Inicialização de variáveis
@@ -46,6 +47,7 @@ void BinaryOperations::initialize() {
   BinaryOperations::add_type(BinaryOperation::MULT, binary_mult_get_type);
   BinaryOperations::add_type(BinaryOperation::DIV, binary_div_get_type);
   BinaryOperations::add_type(BinaryOperation::MOD, binary_mod_get_type);
+  BinaryOperations::add_type(BinaryOperation::BITWISE_OR, binary_bit_or_get_type);
 
   // TODO: Restante das operações
 };
---
+++ src/operations/binary/implementations/bit_or.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "../../../helpers/numeric.hpp"
+#include "../binary.hpp"
+
+Type binary_bit_or_get_type(const Type& left, const Type& right, int line);
