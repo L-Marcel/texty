f4da6890c0bff56ea8ee9d114741f37191675a65
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2026-06-14 13:09:17 -0300

    feat: adicionando unary

--- src/operations/unary/implementations/unary.cpp
+++ src/operations/unary/implementations/unary.cpp
@@ -32,17 +32,23 @@ Type unary_rev_get_type(const Type& node, int line) {
 }
 
 Type unary_ref_get_type(const Type& node, int line) {
-  if (check_if_is_pointer(node)) {
-    return Type(TypeKind::POINTER, new Type(node));
-  } else {
+  if (node == Type(TypeKind::VOID)) {
     throw error("operação unária '&' não definida para o tipo (" +
                     node.to_string() + ")",
                 line);
-  }
+  };
+
+  return Type(TypeKind::POINTER, new Type(node));
 }
 
 Type unary_deref_get_type(const Type& node, int line) {
-  return Type(TypeKind::POINTER, new Type(node));
+  if (check_if_is_pointer(node) && node.inner_type != nullptr) {
+    return *node.inner_type;
+  } else {
+    throw error("operação unária '*' não definida para o tipo (" +
+                    node.to_string() + ")",
+                line);
+  }
 }
 
 Type unary_increment_get_type(const Type& node, int line) {
